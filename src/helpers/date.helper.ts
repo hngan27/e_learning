20e6582c5fda971f709fc2c3e2bd2400834b2f72
ctpanh <ctpanh03@gmail.com>
2024-08-22 16:26:08 +0700

    feat: view lessons

--- src/helpers/date.helper.ts
+++ src/helpers/date.helper.ts
@@ -1,5 +1,3 @@
-import { time } from 'console';
-
 export const formatDateTime = (date: Date) => {
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
